c82a27e50aebeab59ba93314c4589265db66556d
vtu25490-spec <vtu25490@veltech.edu.in>
2026-02-11 10:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.39%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,47 @@
+
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.push(2);
+        System.out.println(myQueue.peek());  // 1
+        System.out.println(myQueue.pop());   // 1
+        System.out.println(myQueue.empty()); // false
+    }
+}
