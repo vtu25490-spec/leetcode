1bbe57f0cd19c6ee5080dc9f2df88e5e143e8d8c
vtu25490-spec <vtu25490@veltech.edu.in>
2026-02-11 11:17:37 +0530

    Time: 3 ms (99.69%) | Memory: 94.4 MB (79.16%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+     if (head == null || head.next == null){
+        return true;
+     }   
+     ListNode slow = head;
+     ListNode fast = head;
+     while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+     }
+     ListNode prev = null;
+     while (slow != null) {
+        ListNode nextNode = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = nextNode;
+     }
+     ListNode left = head;
+     ListNode right = prev;
+     while (right != null) {
+        if (left.val != right.val){
+            return false;
+        }
+        left = left.next;
+        right = right.next;
+     }
+     return true;
+
+    }
+}
