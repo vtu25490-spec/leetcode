f693a25e5ef2b2e9ee5a6d4150f6198241beb5dd
vtu25490-spec <vtu25490@veltech.edu.in>
2026-05-01 17:23:20 +0530

    Time: 50 ms (87.04%) | Memory: 88.8 MB (83.93%) - LeetSync

---
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    private int[] counts;
+    private int[] indexes;
+
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        counts = new int[n];
+        indexes = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            indexes[i] = i;
+        }
+
+        mergeSort(nums, 0, n - 1);
+
+        List<Integer> result = new ArrayList<>();
+        for (int count : counts) {
+            result.add(count);
+        }
+        return result;
+    }
+
+    private void mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+        int rightCount = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[indexes[j]] < nums[indexes[i]]) {
+                temp[k++] = indexes[j++];
+                rightCount++;
+            } else {
+                counts[indexes[i]] += rightCount;
+                temp[k++] = indexes[i++];
+            }
+        }
+
+        while (i <= mid) {
+            counts[indexes[i]] += rightCount;
+            temp[k++] = indexes[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = indexes[j++];
+        }
+
+        for (int p = 0; p < temp.length; p++) {
+            indexes[left + p] = temp[p];
+        }
+    }
+}
