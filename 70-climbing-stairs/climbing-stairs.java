1ada27b5da0fa0e0b24c4b36e25ea0e9f5bb999d
vtu25490-spec <vtu25490@veltech.edu.in>
2026-05-01 17:27:06 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.43%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int climbStairs(int n) {
+
+        // Base cases
+        if (n <= 2) {
+            return n;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        // Fibonacci approach
+        for (int i = 3; i <= n; i++) {
+
+            int current = first + second;
+
+            first = second;
+            second = current;
+        }
+
+        return second;
+    }
+}
