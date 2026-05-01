7b553f656ec61761b7b76f72e7143e0dc86b94b5
vtu25490-spec <vtu25490@veltech.edu.in>
2026-05-01 16:34:04 +0530

    Time: 7 ms (98.88%) | Memory: 46.5 MB (69.88%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,31 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        // Search in left and right subtree
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // If both sides return non-null, root is LCA
+        if (left != null && right != null) {
+            return root;
+        }
+
+        // Otherwise return non-null value
+        return (left != null) ? left : right;
+    }
+}
